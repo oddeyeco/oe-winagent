18906e8a114d2be7102f0b599d608b4a39842006
hakhverdyan <aramhakhverdyan@gmail.com>
2018-02-08 12:01:56 +0400

    Memory detailed monitoring metrics is added

--- service/checkers/system_memory_stats_checker.cpp
+++ service/checkers/system_memory_stats_checker.cpp
@@ -9,29 +9,41 @@ SystemMemoryStatsChecker::SystemMemoryStatsChecker()
 
 void SystemMemoryStatsChecker::Initialize()
 {
-    double dConfigHighValue = -1;
-    double dConfigSevereValue = -1;
+    double dConfHighVal = -1;
+    double dConfSevereVal = -1;
 
     bool bStaticAlertingEnabled = ConfigSection().Value<bool>("static_enabled", false);
     if( bStaticAlertingEnabled )
     {
-        dConfigHighValue = ConfigSection().Value<double>("high", -1);
-        dConfigSevereValue = ConfigSection().Value<double>("severe", -1);
+        dConfHighVal   = ConfigSection().Value<double>("high", -1);
+        dConfSevereVal = ConfigSection().Value<double>("severe", -1);
     }
 
-    bool bPerCoreEnabled =  ConfigSection().Value<bool>("percore_stats", false);
     bool bDetailedEnabled =  ConfigSection().Value<bool>("detailed_stats", false);
 
-    AddCounterMetricChecker( "Memory Committed Bytes In Use",   "\\Memory%1\\% Committed Bytes In Use", EMetricDataType::Percent, "SYSTEM", 0, dConfigHighValue, dConfigSevereValue);
-    AddCounterMetricChecker( "Memory Available Bytes",          "\\Memory%1\\Available Bytes",          EMetricDataType::Counter, "SYSTEM" );
-    AddCounterMetricChecker( "Memory Cache Bytes",              "\\Memory%1\\Cache Bytes",              EMetricDataType::Counter, "SYSTEM" );
-    AddCounterMetricChecker( "Memory Committed Bytes",          "\\Memory%1\\Committed Bytes",          EMetricDataType::Counter, "SYSTEM" );
-    AddCounterMetricChecker( "Memory Page Reads/sec",           "\\Memory%1\\Page Reads/sec",           EMetricDataType::Counter, "SYSTEM" );
-    AddCounterMetricChecker( "Memory Page Writes/sec",          "\\Memory%1\\Page Writes/sec",          EMetricDataType::Counter, "SYSTEM" );
-    AddCounterMetricChecker( "Memory Pages Input/sec",          "\\Memory%1\\Pages Input/sec",          EMetricDataType::Counter, "SYSTEM" );
-    AddCounterMetricChecker( "Memory Page Pages Output/sec",    "\\Memory%1\\Pages Output/sec",         EMetricDataType::Counter, "SYSTEM" );
-    AddCounterMetricChecker( "Memory Page Pages/sec",           "\\Memory%1\\Pages/sec",                EMetricDataType::Counter, "SYSTEM" );
-    AddCounterMetricChecker( "Memory Page Pool Nonpaged Bytes", "\\Memory%1\\Pool Nonpaged Bytes",      EMetricDataType::Counter, "SYSTEM" );
-    AddCounterMetricChecker( "Memory Page Pool Paged Bytes",    "\\Memory%1\\Pool Paged Bytes",         EMetricDataType::Counter, "SYSTEM" );
+    AddCounterMetricChecker( "memory_committed_bytes_in_use",    "\\Memory%1\\% Committed Bytes In Use",     EMetricDataType::Percent, "SYSTEM", 0, dConfHighVal, dConfSevereVal);
+    AddCounterMetricChecker( "memory_available_bytes",           "\\Memory%1\\Available Bytes",              EMetricDataType::Counter, "SYSTEM" );
+    AddCounterMetricChecker( "memory_cache_bytes",               "\\Memory%1\\Cache Bytes",                  EMetricDataType::Counter, "SYSTEM" );
+    AddCounterMetricChecker( "memory_committed_bytes",           "\\Memory%1\\Committed Bytes",              EMetricDataType::Counter, "SYSTEM" );
+    AddCounterMetricChecker( "memory_page_reads/sec",            "\\Memory%1\\Page Reads/sec",               EMetricDataType::Rate,    "SYSTEM" );
+    AddCounterMetricChecker( "memory_page_writes/sec",           "\\Memory%1\\Page Writes/sec",              EMetricDataType::Rate,    "SYSTEM" );
+    AddCounterMetricChecker( "memory_pages_input/sec",           "\\Memory%1\\Pages Input/sec",              EMetricDataType::Rate,    "SYSTEM" );
+    AddCounterMetricChecker( "memory_page_pages_output/sec",     "\\Memory%1\\Pages Output/sec",             EMetricDataType::Rate,    "SYSTEM" );
+    AddCounterMetricChecker( "memory_page_pages/sec",            "\\Memory%1\\Pages/sec",                    EMetricDataType::Rate,    "SYSTEM" );
+    AddCounterMetricChecker( "memory_page_pool_nonpaged_bytes",  "\\Memory%1\\Pool Nonpaged Bytes",          EMetricDataType::Counter, "SYSTEM" );
+    AddCounterMetricChecker( "memory_page_pool_paged_bytes",     "\\Memory%1\\Pool Paged Bytes",             EMetricDataType::Counter, "SYSTEM" );
 
+    if( bDetailedEnabled )
+    {
+        //
+        //  Add extra checkers
+        //
+        AddCounterMetricChecker( "memory_commit_limit",          "\\Memory%1\\Commit Limit",                 EMetricDataType::Counter, "SYSTEM" );
+        AddCounterMetricChecker( "memory_cache_bytes_peak",      "\\Memory%1\\Cache Bytes Peak",             EMetricDataType::Counter, "SYSTEM" );
+        AddCounterMetricChecker( "system_cache_resident_bytes",  "\\Memory%1\\System Cache Resident Bytes",  EMetricDataType::Counter, "SYSTEM" );
+        AddCounterMetricChecker( "system_code_resident_bytes",   "\\Memory%1\\System Code Resident Bytes",   EMetricDataType::Counter, "SYSTEM" );
+        AddCounterMetricChecker( "system_code_total_bytes",      "\\Memory%1\\System Code Total Bytes",      EMetricDataType::Counter, "SYSTEM" );
+        AddCounterMetricChecker( "system_driver_resident_bytes", "\\Memory%1\\System Driver Resident Bytes", EMetricDataType::Counter, "SYSTEM" );
+        AddCounterMetricChecker( "system_driver_total_bytes",    "\\Memory%1\\System Driver Total Bytes",    EMetricDataType::Counter, "SYSTEM" );
+    }
 }
